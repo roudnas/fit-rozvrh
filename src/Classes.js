ca18ab63659d58bf00e40160ff4805a86e6f7ee4
roudnas <dominikroudny@seznam.cz>
2021-09-09 10:11:49 +0200

    initial

---
+++ src/Classes.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Classes = ({ dayIndex, timeIndex, studentClasses, color, show }) => {
+  const getBadgeClasses = (color) => {
+    return `badge p-2 my-1 hodina rounded-pill bg-${color} text-dark`;
+  };
+
+  return (
+    <>
+      {studentClasses[dayIndex][timeIndex] !== "" && show.includes(color) ? (
+        <span className={getBadgeClasses(color)}>
+          <p className="m-0">{studentClasses[dayIndex][timeIndex]}</p>
+        </span>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default Classes;
