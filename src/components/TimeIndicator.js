df029f890ed1611cc011e9cafb08cc26d6b7b1a0
Jiri Javora <javora.jiri@gmail.com>
2022-10-06 16:12:22 +0200

    Made time pin move every minute

--- src/components/TimeIndicator.js
+++ src/components/TimeIndicator.js
@@ -1,9 +1,17 @@
-import {APP_PADDING_X} from '../App';
-import {TIME_OFFSET_PX} from './Class';
+import { useState, useEffect } from 'react';
 
-export function TimeIndicator({dims}) {
-    const currentTime = new Date();
+import { APP_PADDING_X } from '../App';
+import { TIME_OFFSET_PX } from './Class';
+
+export function TimeIndicator({ dims }) {
+    const [currentTime, setCurrentTime] = useState(new Date());
     const sT = currentTime.getHours() + currentTime.getMinutes() / 60;
+
+    useEffect(() => {
+        const minuteRefresher = setInterval(() => { setCurrentTime(new Date()) }, 60000)
+
+        return () => clearInterval(minuteRefresher)
+    }, [])
 
     if (sT < 7.1 || sT > 20.7)
         return null;
@@ -14,8 +22,8 @@ export function TimeIndicator({dims}) {
     const marL = (sT - 7) * fullW + TIME_OFFSET_PX;
 
     return (
-        <div className='time-indicator' style={{marginLeft: marL}}>
-            <div className='time-indicator-pin'/>
+        <div className='time-indicator' style={{ marginLeft: marL }}>
+            <div className='time-indicator-pin' />
         </div>
     )
 }
