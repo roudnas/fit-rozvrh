3d02cdf93fa882834ffde5b8a8790610f966d637
sentisso <sentisso.dev@protonmail.com>
2023-02-28 22:23:15 +0100

    adding PWA support via the Vite PWA plugin (vite-plugin-pwa)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(), viteTsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      }
+    })
+  ],
   build: {
     outDir: 'build',
   },
