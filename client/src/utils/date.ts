e784dcb3aea2bb7a50ba185bc92f76d5bd6a93ff
sentisso <sentisso.dev@protonmail.com>
2023-03-01 01:17:59 +0100

    converting moment.js functionalities to date-fns "equivalents"

--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -1,15 +1,25 @@
-import moment from 'moment';
+import { differenceInCalendarWeeks, getWeek, subMinutes, addMinutes } from 'date-fns';
 
 /**
- * The time data CTU provides is generated in the Europe/Prague timezone (+1 hour).
+ * The time data CTU provides is generated in the Europe/Prague timezone (GMT +1).
  */
-export const UTC_OFFSET = 1;
+export const CTU_TIMEZONE_OFFSET = 1;
 
 /**
- * Get moment's, timezone corrected, date object.
+ * Get "timezone corrected", date object.
  */
 export function getOffsetDate() {
-  return moment().utcOffset(UTC_OFFSET);
+  const date = new Date();
+  // how many minutes are we off from the CTU timezone
+  const offset = date.getTimezoneOffset() + CTU_TIMEZONE_OFFSET * 60;
+
+  if (offset === 0)
+    return date; // no offset, return the original date
+
+  // add or subtract the offset to get the correct date
+  return offset > 0 ?
+    addMinutes(date, Math.abs(offset)) :
+    subMinutes(date, Math.abs(offset));
 }
 
 /**
@@ -17,10 +27,15 @@ export function getOffsetDate() {
  * @param semesterStartTimestamp unix timestamp of the semester start (in milliseconds).
  */
 export function getSemesterWeekNo(semesterStartTimestamp: number) {
-  return 1 + moment.unix(semesterStartTimestamp / 1000)
-    .diff(getOffsetDate(), 'weeks');
+  return 1 + differenceInCalendarWeeks(
+    getOffsetDate(),
+    new Date(semesterStartTimestamp),
+    { weekStartsOn: 1 },
+  );
 }
 
 export function getEvenOddWeek() {
-  return getOffsetDate().week() % 2 === 0 ? 'even' : 'odd';
+  return getWeek(
+    getOffsetDate(), { weekStartsOn: 1 }
+  ) % 2 === 1 ? 'even' : 'odd';
 }
