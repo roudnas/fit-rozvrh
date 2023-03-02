d823e8a4eb052aa28372a4b9e1929334ebc364d9
Dominik Roudny <dominik.roudny@blueghost.cz>
2023-03-02 01:25:23 +0100

    Returns back the port? wtf.

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use("/data", DataRouter);
-app.listen(() => {
+app.listen(port,() => {
     console.log("App listening.")
 })
 
