d4ebedc843e748addd57b8051a376f772351abe0
Jiri Javora <javorji1@fit.cvut.cz>
2023-03-07 01:32:41 +0100

    added dropdown search

--- client/src/components/VictimDropdown.tsx
+++ client/src/components/VictimDropdown.tsx
@@ -1,12 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import useVictim from '../hooks/useVictim';
 
+import useVictim from '../hooks/useVictim';
 
 export function VictimDropdown() {
   const { setVictimId, people } = useVictim();
+  const [victimSearch, setVictimSearch] = useState<string>('');
+  const [displayVictimDropdown, setDisplayVictimDropdown] =
+    useState<boolean>(false);
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const firstVictimRef = useRef<HTMLButtonElement>(null);
+  const lastVictimRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (displayVictimDropdown) {
+      searchRef?.current?.focus();
+    } else {
+      setVictimSearch('');
+    }
+  }, [displayVictimDropdown]);
+
+  const victimSearchLowerCase = victimSearch.toLowerCase();
 
   return (
-    <Dropdown>
+    <Dropdown
+      focusFirstItemOnShow={false}
+      onToggle={(show) => {
+        setDisplayVictimDropdown(show);
+      }}
+      show={displayVictimDropdown}
+    >
       <Dropdown.Toggle
         className="text-bold bg-black"
         variant="secondary"
@@ -14,17 +38,79 @@ export function VictimDropdown() {
       >
         VICTIM
       </Dropdown.Toggle>
-      <Dropdown.Menu renderOnMount>
-        {people.map((person, i) => (
-          <Dropdown.Item
-            key={i}
-            onClick={() => {
-              setVictimId(person.id);
+      <Dropdown.Menu
+        renderOnMount
+        style={{ maxHeight: '80vh', overflowY: 'auto' }}
+      >
+        <Dropdown.Header>
+          <input
+            ref={searchRef}
+            value={victimSearch}
+            onChange={(e) => {
+              setVictimSearch(e.target.value);
             }}
-          >
-            {person.name}
-          </Dropdown.Item>
-        ))}
+            onKeyDown={(e) => {
+              if (e.key === 'ArrowDown') {
+                firstVictimRef?.current?.focus();
+                e.preventDefault();
+                e.stopPropagation();
+                return;
+              }
+              if (e.key === 'ArrowUp') {
+                if (lastVictimRef) {
+                  lastVictimRef.current?.focus();
+                  e.preventDefault();
+                  e.stopPropagation();
+                  return;
+                }
+                firstVictimRef?.current?.focus();
+                e.preventDefault();
+                e.stopPropagation();
+                return;
+              }
+              if (e.key === 'Enter') {
+                firstVictimRef?.current?.click();
+                e.stopPropagation();
+                e.preventDefault();
+              }
+            }}
+          />
+        </Dropdown.Header>
+        {people
+          .filter((person) =>
+            person.name.toLowerCase().startsWith(victimSearchLowerCase),
+          )
+          .map((person, i, { length }) => {
+            const isFirst = i === 0;
+            const isLast = i === length - 1;
+
+            return (
+              <Dropdown.Item
+                key={person.name}
+                onClick={() => {
+                  setVictimId(person.id);
+                }}
+                ref={
+                  isFirst ? firstVictimRef : (isLast ? lastVictimRef : undefined)
+                }
+                onKeyDown={(e) => {
+                  if (isFirst && e.key === 'ArrowUp') {
+                    searchRef.current?.focus();
+                    e.preventDefault();
+                    e.stopPropagation();
+                    return;
+                  }
+                  if (isLast && e.key === 'ArrowDown') {
+                    firstVictimRef?.current?.focus();
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }
+                }}
+              >
+                {person.name}
+              </Dropdown.Item>
+            );
+          })}
       </Dropdown.Menu>
     </Dropdown>
   );
