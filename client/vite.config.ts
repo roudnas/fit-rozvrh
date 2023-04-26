c14fe0cb22fb73b4ce092c0b75f251d50c7f9e95
roudnas <dominik@roudny.eu>
2023-04-26 22:34:39 +0200

    Fixed linting errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,44 +1,44 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-  return defineConfig({
-    plugins: [
-      react(), viteTsconfigPaths(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        workbox: {
-          runtimeCaching: [
-            // https://developer.chrome.com/docs/workbox/modules/workbox-strategies/#stale-while-revalidate
-            {
-              urlPattern: `${process.env.VITE_API_BASE}/data`,
-              handler: 'StaleWhileRevalidate',
-              options: {
-                cacheName: 'api-data-cache',
-                expiration: {
-                  maxEntries: 1,
-                  maxAgeSeconds: 60 * 60 * 24 * 3, // 3 days
+    return defineConfig({
+        plugins: [
+            react(), viteTsconfigPaths(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                workbox: {
+                    runtimeCaching: [
+                        // https://developer.chrome.com/docs/workbox/modules/workbox-strategies/#stale-while-revalidate
+                        {
+                            urlPattern: `${process.env.VITE_API_BASE}/data`,
+                            handler: 'StaleWhileRevalidate',
+                            options: {
+                                cacheName: 'api-data-cache',
+                                expiration: {
+                                    maxEntries: 1,
+                                    maxAgeSeconds: 60 * 60 * 24 * 3, // 3 days
+                                },
+                            }
+                        },
+                    ],
                 },
-              }
-            },
-          ],
-        },
-        includeAssets: [
-          '**/*.png', '**/*.ico', '**/*.svg', '**/*.gif', '**/*.jpg', '**/*.jpeg',
+                includeAssets: [
+                    '**/*.png', '**/*.ico', '**/*.svg', '**/*.gif', '**/*.jpg', '**/*.jpeg',
+                ],
+                devOptions: {
+                    enabled: true,
+                },
+            }),
         ],
-        devOptions: {
-          enabled: true,
+        build: {
+            outDir: 'build',
         },
-      }),
-    ],
-    build: {
-      outDir: 'build',
-    },
-  });
+    });
 }
